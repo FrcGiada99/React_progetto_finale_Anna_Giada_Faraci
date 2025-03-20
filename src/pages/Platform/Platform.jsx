146791ba5c49e3b891012dfa31d76eaa9157f132
Anna Giada Faraci <fgiada84@gmail.com>
2025-03-20 12:02:19 +0100

    ultima modifica

--- src/pages/Platform/Platform.jsx
+++ src/pages/Platform/Platform.jsx
@@ -6,7 +6,6 @@ import Sidebar from "../../components/Sidebar";
 const Platform = () => {
   const { id } = useParams();
   const [games, setGames] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [platforms, setPlatforms] = useState([]);
   const [platformName, setPlatformName] = useState("");
 
@@ -32,17 +31,11 @@ const Platform = () => {
 
   useEffect(() => {
     const fetchGamesForPlatform = async () => {
-      try {
-        const response = await fetch(
-          `https://api.rawg.io/api/games?key=f5fa696e3b9146d7aa1e40a30e309f02&platforms=${id}`
-        );
-        const data = await response.json();
-        setGames(data.results);
-      } catch (error) {
-        console.error("Errore nel recupero dei giochi:", error);
-      } finally {
-        setLoading(false);
-      }
+      const response = await fetch(
+        `https://api.rawg.io/api/games?key=f5fa696e3b9146d7aa1e40a30e309f02&platforms=${id}`
+      );
+      const data = await response.json();
+      setGames(data.results);
     };
     fetchGamesForPlatform();
   }, [id]);
@@ -54,18 +47,14 @@ const Platform = () => {
           <Sidebar />
         </div>
         <div className="col-md-9">
-          <h3> {platformName || "Loading..."}</h3>{" "}
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <div className="row">
-              {games.map((game) => (
-                <div key={game.id} className="col-md-4 mb-4">
-                  <GameCard game={game} />
-                </div>
-              ))}
-            </div>
-          )}
+          <h3>{platformName}</h3>
+          <div className="row">
+            {games.map((game) => (
+              <div key={game.id} className="col-md-4 mb-4">
+                <GameCard game={game} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
