bd40696addd837d73b7b66e172d7eddfdd667227
Anna Giada Faraci <fgiada84@gmail.com>
2025-03-25 21:20:59 +0100

    eliminato scroll su genres

--- src/pages/Genre/index.jsx
+++ src/pages/Genre/index.jsx
@@ -4,34 +4,36 @@ import Sidebar from "../../components/Sidebar";
 import GameCard from "../../components/GameCard";
 
 export default function Genre() {
-    const [games, setGames] = useState([]);
-    const { genre } = useParams();
+  const [games, setGames] = useState([]);
+  const { genre } = useParams();
 
-    useEffect(() => {
-        const fetchGenre = async () => {
-            const response = await fetch(`https://api.rawg.io/api/games?key=f5fa696e3b9146d7aa1e40a30e309f02&genres=${genre}&page=1`);
-            const json = await response.json();
-            setGames(json.results);
-        };
-        fetchGenre();
-    }, [genre]); 
+  useEffect(() => {
+    const fetchGenre = async () => {
+      const response = await fetch(
+        `https://api.rawg.io/api/games?key=f5fa696e3b9146d7aa1e40a30e309f02&genres=${genre}&page=1`
+      );
+      const json = await response.json();
+      setGames(json.results);
+    };
+    fetchGenre();
+  }, [genre]);
 
-    return (
-        <div className="d-flex">
-            <div className="bg-light border-end p-3" style={{ width: "250px", height: "100vh", overflowY: "auto" }}>
-                <Sidebar />
+  return (
+    <div className="d-flex">
+      <div className="bg-light border-end p-3" style={{}}>
+        <Sidebar />
+      </div>
+
+      <div className="container-fluid ms-3 flex-grow-1">
+        <h1>{genre} Games</h1>
+        <div className="row">
+          {games.map((game) => (
+            <div key={game.id} className="col-md-3 mb-4">
+              <GameCard game={game} />
             </div>
-    
-            <div className="container-fluid ms-3 flex-grow-1">
-                <h1>{genre} Games</h1>
-                <div className="row">
-                    {games.map((game) => (
-                        <div key={game.id} className="col-md-3 mb-4">
-                            <GameCard game={game} />
-                        </div>
-                    ))}
-                </div>
-            </div>
+          ))}
         </div>
-    );
-}    
+      </div>
+    </div>
+  );
+}
